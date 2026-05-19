447cbc379c9de854403f94bb1fbfa8ea77dd9bf0
Ayush0080 <payush512@gmail.com>
2026-05-19 15:00:05 +0530

    19-May

---
+++ Day-12/destroy-cluster.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "==============================="
+echo "STEP-1: Destroy EKS Cluster"
+echo "==============================="
+cd EKS_terraform-manifests_with_addons
+terraform init
+terraform destroy -auto-approve
+
+echo
+echo "🧹 Cleaning up local Terraform cache..."
+rm -rf .terraform .terraform.lock.hcl
+
+echo
+echo "==============================="
+echo "STEP-2: Destroy VPC"
+echo "==============================="
+cd ../VPC_terraform-manifests
+terraform init
+terraform destroy -auto-approve
+
+echo
+echo "🧹 Cleaning up local Terraform cache..."
+rm -rf .terraform .terraform.lock.hcl
+
+echo
+echo "✅ EKS Cluster and VPC destroyed and cleaned up successfully!"
---
+++ Day-12/vpc_terraform-manifests/terraform.tfvars
@@ -0,0 +1,11 @@
+
+environment_name = "dev"
+aws_region       = "ap-south-1"
+
+vpc_cidr = "10.0.0.0/16"
+
+subnet_newbits = 8
+ 
+tags = {
+  Terraform   = "true"
+}
